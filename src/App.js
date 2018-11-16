8fe53363061c36ae425482af8a395ec0cc159d86
crojas <c.rojas.latorre@gmail.com>
2018-11-16 14:49:12 -0300

    update msg

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            victor deja el azucar
+            Prueba nodejs + docker + travis + aws eb
           </p>
           <a
             className="App-link"
